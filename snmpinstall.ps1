87022ce2090d34d1c2a730a1717e25c8c10d076b
Ren <renato.regalado@gmail.com>
2017-11-29 12:40:25 -0600

    removed invoke-command
    used enter-pssession instead.

--- snmpinstall.ps1
+++ snmpinstall.ps1
@@ -1,3 +1,6 @@
+#get credential
+$cred = Get-Credential;
+
 #get server names from input file;
 
 $computers = get-content .\inputs\servers.txt;
@@ -22,14 +25,21 @@ foreach ($c in $computers) {
 
     #test connection with the server
     If (Test-connection -ComputerName $c -count 1) {
-
+		#enter a powershell session for the computer names
+		try {
+            enter-pssession -computerName $c -Credential $cred;
+            "successfully opened a PS session on $c" | out-file $log -Append;
+            }
+        catch {
+        "Could not open a PS session on $c" |  out-file $log -Append;
+        }
         #get SNMP service and assign to variable
-        $SnmpInstalled = invoke-command -ComputerName $c -ScriptBlock{get-wmiobject Win32_Service -filter "Name='SNMP'"}
+        $SnmpInstalled =  get-wmiobject Win32_Service -filter "Name='SNMP'"
         
         #if it's not installed, install it
         if (!($SnmpInstalled)){
             try {
-            invoke-command -computername $c -Scriptblock {Install-WindowsFeature -IncludeAllSubFeature 'SNMP-Service'};
+            Install-WindowsFeature -IncludeAllSubFeature 'SNMP-Service';
             "The SNMP installed on $c" | out-file $log -Append 
             }
             Catch {
@@ -42,16 +52,16 @@ foreach ($c in $computers) {
             }
 
         #check Server if community string already exists
-        invoke-command -ComputerName $c -ScriptBlock {
+        
         $regpath = "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities";
         $dword = get-item -path $regpath | select -ExpandProperty property;
-        }
+        
         
         #if it does not exist, run psexec to install the registry keys.
         if ($dword -eq $null) {
 
             try {
-            invoke-command -computerName $c -ScriptBlock { regedit.exe /S "\\[SERVER_LOCATION_NAME]\[REG_FILE_NAME];"}
+            regedit.exe /S "\\[SERVER_NAME]\[REGFILE_DIRECTORY]"
             "Ran the Registry key file successfully on $c `r`n" | out-file $log -Append;
             }
 
@@ -64,7 +74,8 @@ foreach ($c in $computers) {
         Else {
         "SNMP community string has already been set up on $c `r`n" | out-file $log -Append;
         }
-        
+		
+      exit-pssession;  
     }
 
     Else {
